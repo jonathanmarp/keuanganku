81fa4c6f140fbbe9404c628e72deafb4a1268a65
Jonathan Laferen <60574415+jonathanmarp@users.noreply.github.com>
2022-05-24 10:03:07 +0700

    Sudah menambahkan API
    Saya sudah menambahkan api untuk keuangan, perhutangan, dan juga user

--- database/migrations/2022_05_23_092632_create_debt_users_table.php
+++ database/migrations/2022_05_23_092632_create_debt_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateDebtUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('debt_users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string("key");
+            $table->string("type");
+            $table->double("size");
+            $table->boolean("paid");
+            $table->double("create");
+            $table->double("has_paid");
             $table->timestamps();
         });
     }
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('debt_users');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\DebtUsers;
+use App\Http\Controllers\MoneyUser;
+use App\Http\Controllers\Users;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,7 +15,53 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::get("/", [Users::class, "index"]);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+// Create method
+// Group user
+Route::group(["prefix" => "user"], function () use ($router) {
+    // Create
+    $router->get("/create/{email}/{username}/{password}/", [Users::class, "create"]);
+
+    // Login
+    $router->get("/login/{email}/{username}/{password}/", [Users::class, "login"]);
+
+    // Delete
+    $router->get("/delete/{id}/{key}/{email}/{username}/{password}/", [Users::class, "delete"]);
+
+    // Update
+    $router->get("/update/{id}/{key}/{email}/{username}/{password}/{newusername}", [Users::class, "update"]);
+});
+
+// Group MoneyUser
+Route::group(["prefix" => "money"], function () use ($router) {
+    // Create
+    $router->get("/create/{key}/{type}/{size}/", [MoneyUser::class, "create"]);
+
+    // Delete
+    $router->get("/delete/{key}/{id}/", [MoneyUser::class, "delete"]);
+
+    // Update
+    $router->get("/update/{key}/{id}/{size}", [MoneyUser::class, "update"]);
+
+    // All
+    $router->get("/all/{key}/", [MoneyUser::class, "all"]);
+});
+
+// Group MoneyUser
+Route::group(["prefix" => "debt"], function () use ($router) {
+    // Create
+    $router->get("/create/{key}/{type}/{size}/", [DebtUsers::class, "create"]);
+
+    // Delete
+    $router->get("/delete/{key}/{id}/", [DebtUsers::class, "delete"]);
+
+    // Update
+    $router->get("/update/{key}/{id}/{size}", [DebtUsers::class, "update"]);
+
+    // Update
+    $router->get("/paid/{key}/{id}", [DebtUsers::class, "paid"]);
+
+    // All
+    $router->get("/all/{key}/", [DebtUsers::class, "all"]);
 });
